e71403de6d4064e801b09c02f223321195525d8c
Gaurav-Shah05 <gauravmshah1205@gmail.com>
2025-03-26 00:36:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProprCalculator from './components/ProprCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <ProprCalculator />
+    </div>
+  );
+}
+
+export default App;
